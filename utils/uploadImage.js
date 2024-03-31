832a02785b69d461160c7b85fe28d314e5069eff
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-03-31 18:51:48 +0100

    Create Cloudinary uploader

---
+++ utils/uploadImage.js
@@ -0,0 +1,18 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+async function uploadImage(image) {
+  try{
+    const result = await cloudinary.uploader.upload(image);
+    return result.secure_url;
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+module.exports = { uploadImage };
