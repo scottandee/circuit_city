a3bdf309628603854163e437b89bae777a4d217b
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-04-04 09:08:54 +0100

    Update require path

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const appController = require('../controllers/AppController');
+const appController = require('../controllers/appController');
 
 const router = express.Router();
 
